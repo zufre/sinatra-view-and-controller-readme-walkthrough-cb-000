bb23fb0f8519b942e1c8feba3e473daa921178f3
Cedric David <cedric.david@btopenworld.com>
2018-09-19 06:17:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get '/friends' do
     # Write your code here!
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
- 
+
   erb :friends
 
 
